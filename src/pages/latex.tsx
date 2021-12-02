f70b503f9141dae283ac71b91de833f823161e14
poteitoo <1105tigery@gmail.com>
2021-12-02 19:32:16 +0900

    [Add] getInitialProps to read katex command dict

--- src/pages/latex.tsx
+++ src/pages/latex.tsx
@@ -1,5 +1,25 @@
-import { LaTexEditor } from '../components/templates/LaTexEditor';
+import { NextPage } from 'next';
 
-const Latex = () => <LaTexEditor />;
+import {
+  LaTexEditor,
+  LaTexEditorTemplate,
+} from '../components/templates/LaTexEditor';
+
+const Latex: NextPage<LaTexEditorTemplate> = (props) => (
+  <LaTexEditor katexCommands={props.katexCommands} />
+);
+
+Latex.getInitialProps = async () => {
+  const katexCommands = await import('../../public/katex_commands.json')
+    .then((d) => Object.values(d.default))
+    .then((arr) => arr.map((d, i) => ({ ...d, id: i })))
+    .then((arr) =>
+      arr.length > 0
+        ? arr.reduce((d, x) => ({ ...d, [x.id]: x }), { 0: arr[0] })
+        : {}
+    );
+
+  return { katexCommands };
+};
 
 export default Latex;
