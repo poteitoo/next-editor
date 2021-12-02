e60550195bbbb5594cdb743cdd89f31b49a94683
poteitoo <1105tigery@gmail.com>
2021-12-02 19:27:16 +0900

    [Add] search package & its hook

---
+++ src/hooks/useSearchKatexCommand.tsx
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { useDebounce } from 'use-debounce';
+
+import { Katex } from '../components/organisms/SearchModal';
+
+type UseSearchKatexCommandProps = Katex[];
+
+export const useSearchKatexCommand = (
+  katexDictionary: UseSearchKatexCommandProps
+) => {
+  const [searchWord, setSearchWord] = useState('');
+  const [results, setResults] = useState<Katex[]>([]);
+  const [searchText] = useDebounce(searchWord, 500);
+
+  const fuse = useCallback(async () => {
+    const Fuse = (await import('fuse.js')).default;
+    const options = { keys: ['src', 'symb'] };
+    const index = Fuse.createIndex(options.keys, katexDictionary);
+    return new Fuse(katexDictionary, options, index);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const fuseSearch = async () => {
+      const f = await fuse();
+      const res = f.search(searchText);
+      setResults(res.map((arr) => arr.item));
+    };
+    fuseSearch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchText]);
+
+  return { results, setSearchWord };
+};
